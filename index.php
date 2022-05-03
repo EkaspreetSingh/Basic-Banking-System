176935533ebd8f0f2414d21d369ab2c0fc859e88
EkaspreetSingh <75095366+EkaspreetSingh@users.noreply.github.com>
2022-05-03 16:50:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GRIP_bank</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ramaraja&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+</head>
+<body>
+    <img src="bg.jpg" alt="background" class="bg">
+
+    <div class="navbar">
+        <img src="bank.png" alt="icon"> 
+        <h2>GRIP Bank</h2>
+        <ul>
+            <li><a href="contact.php">Contact Me</a></li>
+            <li><a href="about.html">About Me</a></li>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </div>
+    <div class="container">
+
+        <a href="transfer.php"><div class="button">
+            <div><img src="money.png" alt="icon"></div>
+            TRANSFER MONEY
+        </div></a>
+
+        <a href="users.php"><div class="button">
+        <div><img src="user.png" alt="icon"></div>
+        VIEW ALL USERS
+        </div></a>
+
+        <a href="history.php"><div class="button">
+        <div><img src="history.png" alt="icon"></div>
+        VIEW TRANSACTION HISTORY
+    </div></a>
+
+    </div>
+        
+    <footer class="copyright2">
+        <p>©2022. Made by Ekaspreet Singh</p>
+        <p>For the project of Sparks Foundation</p>
+    </footer>
+
+    <script src = "index.js"></script>
+</body>
+</html>
